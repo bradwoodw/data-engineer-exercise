1563ac25dc77a6b4c934f9fe3f69c23eacb1f3a9
bradwoodw <41024722+bradwoodw@users.noreply.github.com>
2018-07-10 23:24:18 +0100

    Update weatherDDLandTableLoad.sql

--- working_folder/weatherDDLandTableLoad.sql
+++ working_folder/weatherDDLandTableLoad.sql
@@ -11,25 +11,25 @@ CREATE TABLE IF NOT EXISTS public.city (
    	longitude         numeric(6,2),
    	location_type     char(5),
    	title             char(50),
-   	woeid             integer,
+   	woeid             integer
 );
 
 CREATE TABLE IF NOT EXISTS public.weather (
 	air_pressure         	numeric(4,2),
 	applicable_date      	date,
-	created              	datetime,
+	created              	timestamp,
 	humidity             	integer,
 	id                   	bigint,
 	max_temp             	numeric(4,2),
 	min_temp             	numeric(4,2),
 	predictability       	integer,
 	the_temp             	numeric(4,2),
-	visibility           	numeric(2,6),
+	visibility           	numeric(6,2),
 	weather_state_abbr   	char(3),
 	weather_state_name   	char(20),
-	wind_direction       	numeric(2,4),
+	wind_direction       	numeric(2,2),
 	wind_direction_compass  char(3),
-	wind_speed              numeric(2,4),
+	wind_speed              numeric(2,2),
 	woeid                   integer
 );
 
